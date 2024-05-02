f0cf07f43bb6a6661f26cb583ed19f6d8cf2fae5
Michael Carver <carvermichael@gmail.com>
2024-05-02 11:24:18 -0500

    adding ArrayList

--- kata/ArrayList.go
+++ kata/ArrayList.go
@@ -1,3 +1,32 @@
 package kata
 
-// TODO
+type ArrayList[T any] struct {
+}
+
+func (l *ArrayList[T]) Length() int {
+
+}
+
+func (l *ArrayList[T]) InsertAt(item T, index int) error {
+
+}
+
+func (l *ArrayList[T]) Remove(item T) bool {
+
+}
+
+func (l *ArrayList[T]) RemoveAt(index int) (T, error) {
+
+}
+
+func (l *ArrayList[T]) Append(item T) {
+
+}
+
+func (l *ArrayList[T]) Prepend(item T) {
+
+}
+
+func (l *ArrayList[T]) Get(index int) (T, error) {
+
+}
--- kata/ArrayList_test.go
+++ kata/ArrayList_test.go
@@ -1,3 +1,11 @@
 package kata
 
-// TODO
+import (
+	. "kata-machine-go/testUtils"
+	"testing"
+)
+
+func TestArrayList(t *testing.T) {
+	list := ArrayList[int]{}
+	TestList(t, &list)
+}
