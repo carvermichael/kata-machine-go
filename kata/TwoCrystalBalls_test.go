8bf65335b07c8ce35580bc48bdcebb20e0d1fe7c
Michael Carver <carvermichael@gmail.com>
2024-05-02 11:11:13 -0500

    better tests for crystalBalls

--- kata/TwoCrystalBalls_test.go
+++ kata/TwoCrystalBalls_test.go
@@ -6,16 +6,31 @@ import (
 	"testing"
 )
 
-func TestTwoCrystalBalls(t *testing.T) {
+// Case: Basic random target case.
+func TestTwoCrystalBalls_Basic(t *testing.T) {
 	g := NewWithT(t)
+	WithNumbers(g, 10000, rand.Intn(10000))
+}
 
-	idx := rand.Intn(10000)
-	data := make([]bool, 10000)
+// Case: the first ball finds the correct floor (w/ sqrt strategy).
+func TestTwoCrystalBalls_FirstBall(t *testing.T) {
+	g := NewWithT(t)
+	WithNumbers(g, 7491, 7224)
+}
 
-	for i := idx; i < 10000; i++ {
+// Case: Basic random target case.
+func TestTwoCrystalBalls_NoBreak(t *testing.T) {
+	g := NewWithT(t)
+	g.Expect(TwoCrystalBalls(make([]bool, 10000))).To(Equal(-1))
+}
+
+func WithNumbers(g *GomegaWithT, size int, target int) {
+
+	data := make([]bool, size)
+
+	for i := target; i < size; i++ {
 		data[i] = true
 	}
 
-	g.Expect(TwoCrystalBalls(data)).To(Equal(idx))
-	g.Expect(TwoCrystalBalls(make([]bool, 10000))).To(Equal(-1))
+	g.Expect(TwoCrystalBalls(data)).To(Equal(target))
 }
